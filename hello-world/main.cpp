2027abca1c512086b9a692a856785018c31dbdbb
Schoolgitdude88 <robertpilkington@westernwyoming.edu>
2025-01-26 00:56:19 +0000

    Ready for grading.

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,7 +1,14 @@
 //
-// Name
-// Date
+// Robert Pilkington    
+// 01/25/2025
 // Hello World Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
