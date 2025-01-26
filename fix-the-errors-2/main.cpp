c5db0f6145f9cbf69852f5a461140761b0df83d8
Schoolgitdude88 <robertpilkington@westernwyoming.edu>
2025-01-26 03:44:05 +0000

    Ready for Grading

--- fix-the-errors-2/main.cpp
+++ fix-the-errors-2/main.cpp
@@ -1,6 +1,6 @@
 //
-// Name
-// Date
+// Robert Pilkington
+// 01/25/2025
 // Fix the Errors #2 Programming Project
 // COSC 1030
 //
@@ -16,7 +16,7 @@ int main()
 
 	float number;
 	int divider;
-	divider = 0;
+	divider = 2;
 
 	cout << "Hi there" << endl;
 	cout << "Please input a number and then hit return" << endl;
