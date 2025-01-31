6db9477ef2925cfc8aef7a11e62ccc20ea4e81a9
Schoolgitdude88 <robertpilkington@westernwyoming.edu>
2025-01-31 05:16:20 +0000

    Ready for Grading

--- restaurant-bill/main.cpp
+++ restaurant-bill/main.cpp
@@ -1,20 +1,38 @@
 //
-// Name
-// Date
+// Robert Pilkington    
+// 01/30/2029
 // Restaurant Bill Programming Project
 // COSC 1030
 //
 
-// Variable declarations
+#include <iostream>
+using namespace std;
 
-// Constants for the tax rate and tip percentage
 
-// Get the amount of the purchase.
+int main ()
+{
+
+// Get the amount of the purchase. 
+double meal = 88.67;
 
 // Calculate the sales tax.
+double tax = meal * 0.0675;
+
+// Constants for the tax rate and tip percentage
+double subtotal = meal + tax;
 
 // Calculate the tip.
+double tip = subtotal * 0.20;
 
 // Calculate the total of the sale.
+double total = subtotal + tip;
 
 // Print information about the sale.
+cout << "Meal: cost " << meal << endl;
+cout << "Tax: " << tax << endl;
+cout << "Tip: " << tip << endl;
+cout << "Total: " << total << endl; 
+ 
+ 
+ return 0;
+}
