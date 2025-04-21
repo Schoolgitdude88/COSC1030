97d03e1ee0e17ef66d6355883b9a9254f87fc71e
Schoolgitdude88 <robertpilkington@westernwyoming.edu>
2025-04-21 02:40:23 +0000

    Ready for grading

--- backward-string/main.cpp
+++ backward-string/main.cpp
@@ -1,7 +1,47 @@
 //
-// Name
-// Date
+// Robert Pilkington
+// 04/20/2025
 // Backward String Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+// func prototype
+void backward(char *);
+
+int main()
+{
+    // define the array to hold input
+    const int SIZE = 80;
+    char input[SIZE];
+
+    // get string from user
+    cout << "Enter a string and I will enter it backward:\n";
+    cin.getline(input, SIZE);
+
+    //display input backward
+    backward(input);
+    return 0;
+
+}
+
+// thebackward function accepts apointer to a C-string and displays that string backward
+
+void backward(char *str)
+{
+    // get the subscript of the last character in the string
+    int last = strlen (str) -1;
+
+    // work backward through the string displying each character from last to first
+    for (int index = last; index >= 0; index --)
+        cout << str[index];
+
+        //end the line
+    cout << endl;
+
+
+}
--- pig-latin/main.cpp
+++ pig-latin/main.cpp
@@ -1,7 +1,43 @@
 //
-// Name
-// Date
+// Robert Pilkington
+// 04/20/2025
 // Pig Latin Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+// func prototype
+void pigLatin(char *);
+
+int main()
+{
+    // define the array to hold input
+    const int SIZE = 80;
+    char input[SIZE];
+
+    // get string from user
+    cout << "Enter a sentence and I will translate it to pig latin:\n";
+    cin.getline(input, SIZE);
+
+    //display input backward
+    pigLatin(input);
+    return 0;
+
+}
+
+void pigLatin(char *sentence)
+{
+const char *vowels = "AEIOUaeiou";
+
+
+
+
+
+
+
+
+
+}
